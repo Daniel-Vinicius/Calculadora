73846a670160664cacb9b6755a5d61dd0b1de3ae
unknown <you@example.com>
2020-09-17 11:43:26 -0300

    1.0

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Calculator from "./main/Calculator";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>
+      <h1>Calculadora</h1>
+      <Calculator />
+    </div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+serviceWorker.unregister();
